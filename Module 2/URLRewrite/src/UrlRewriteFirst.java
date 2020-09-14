ab133840b2f5d6eabe8fa183b8dcecea21f99c65
Sarith <68165093+sarithdm@users.noreply.github.com>
2020-09-14 20:42:07 +0530

    URL Rewrite

---
+++ Module 2/URLRewrite/src/UrlRewriteFirst.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class UrlRewriteFirst extends HttpServlet {
+public void doGet(HttpServletRequest request, HttpServletResponse response){
+        try{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+		String n=request.getParameter("userName");
+        out.print("Welcome "+n);
+        //appending the username in the query string
+        out.print("<a href='UrlRewriteSecond?uname="+n+"'>visit</a>");
+        out.close();
+
+                }catch(Exception e){System.out.println(e);}
+    }
+}
+
---
+++ Module 2/URLRewrite/src/UrlRewriteSecond.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+public class UrlRewriteSecond extends HttpServlet {
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+{
+        try{
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        //getting value from the query string
+        String n=request.getParameter("uname");
+        out.print("Hello "+n);
+        out.close();
+		                }
+		                catch(Exception e){System.out.println(e);}
+		    }
+
+		}
+
+
+
